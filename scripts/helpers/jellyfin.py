667eec65f586608a79d9c0b05093fdc57af83b4d
Nate Harris <n8gr8gbln@gmail.com>
2020-01-14 23:12:53 -0500

    Password functions

--- scripts/helpers/jellyfin.py
+++ scripts/helpers/jellyfin.py
@@ -11,19 +11,22 @@ user_policy = creds.JELLYFIN_USER_POLICY
 token_header = None
 admin_id = None
 
+
 def authenticate():
     global token_header
     global admin_id
     xEmbyAuth = {
-            'X-Emby-Authorization': 'Emby UserId="{UserId}", Client="{Client}", Device="{Device}", DeviceId="{DeviceId}", Version="{Version}", Token="""'.format(
-                UserId="",  # not required, if it was we would have to first request the UserId from the username
-                Client='account-automation',
-                Device=socket.gethostname(),
-                DeviceId=hash(socket.gethostname()),
-                Version=1,
-                Token=""  # not required
-            )}
-    data = {'Username': creds.JELLYFIN_ADMIN_USERNAME, 'Password': creds.JELLYFIN_ADMIN_PASSWORD, 'Pw': creds.JELLYFIN_ADMIN_PASSWORD}
+        'X-Emby-Authorization': 'Emby UserId="{UserId}", Client="{Client}", Device="{Device}", DeviceId="{'
+                                'DeviceId}", Version="{Version}", Token="""'.format(
+            UserId="",  # not required, if it was we would have to first request the UserId from the username
+            Client='account-automation',
+            Device=socket.gethostname(),
+            DeviceId=hash(socket.gethostname()),
+            Version=1,
+            Token=""  # not required
+        )}
+    data = {'Username': creds.JELLYFIN_ADMIN_USERNAME, 'Password': creds.JELLYFIN_ADMIN_PASSWORD,
+            'Pw': creds.JELLYFIN_ADMIN_PASSWORD}
     try:
         res = postWithToken(hdr=xEmbyAuth, method='/Users/AuthenticateByName', data=data).json()
         token_header = {'X-Emby-Token': '{}'.format(res['AccessToken'])}
@@ -44,7 +47,9 @@ def getWithToken(hdr, method, data=None):
 
 
 def post(cmd, params, payload):
-    return requests.post('{}{}?api_key={}{}'.format(JELLYFIN_URL, cmd, JELLYFIN_API_KEY, ("&" + params if params is not None else "")), json=payload)
+    return requests.post(
+        '{}{}?api_key={}{}'.format(JELLYFIN_URL, cmd, JELLYFIN_API_KEY, ("&" + params if params is not None else "")),
+        json=payload)
 
 
 def postWithToken(hdr, method, data=None):
@@ -53,13 +58,45 @@ def postWithToken(hdr, method, data=None):
     return res
 
 
+def makeUser(username):
+    url = '/Users/New'
+    data = {
+        'Name': username
+    }
+    return post(url, None, payload=data)
+
+
+def resetPassword(userId):
+    url = '/Users/{}/Password'.format(userId)
+    data = {
+        'Id': str(userId),
+        'ResetPassword': 'true'
+    }
+    return postWithToken(hdr=token_header, method=url, data=data)
+
+
+def setUserPassword(userId, currentPass, newPass):
+    url = '/Users/{}/Password'.format(userId)
+    data = {
+        'Id': userId,
+        'CurrentPw': currentPass,
+        'NewPw': newPass
+    }
+    return postWithToken(hdr=token_header, method=url, data=data)
+
+
+def updatePolicy(userId, policy):
+    url = '/Users/{}/Policy'.format(userId)
+    return post(url, None, payload=policy)
+
+
 def search(keyword):
-    url = '/Search/Hints?{}'.format(urlencode({'SearchTerm':keyword}))
+    url = '/Search/Hints?{}'.format(urlencode({'SearchTerm': keyword}))
     return getWithToken(hdr=token_header, method=url)['SearchHints']
 
 
 def getLibraries():
-    url = '/Users/{}/Items?{}'.format(str(admin_id), url)
+    url = '/Users/{}/Items'.format(str(admin_id))
     return getWithToken(hdr=token_header, method=url)
 
 
@@ -68,13 +105,15 @@ def updateRating(itemId, upvote):
     print(url)
     return postWithToken(hdr=token_header, method=url)
 
+
 def makePlaylist(name):
     url = '/Playlists?{}'.format(urlencode({'Name': name}))
     print(url)
     return postWithToken(hdr=token_header, method=url)
 
+
 def addToPlaylist(playlistId, itemIds):
-    itemList = ','.join(itemIds)
-    url = '/Playlists/{}/Items?{}'.format(str(playlistId), str(itemList))
+    item_list = ','.join(itemIds)
+    url = '/Playlists/{}/Items?{}'.format(str(playlistId), str(item_list))
     print(url)
     return postWithToken(hdr=token_header, method=url)
